6f0c86fe3e8f6898150c8a8dafed7eceee4df6d4
JpayeTei <jpayetei34@gmail.com>
2026-05-15 22:29:41 +0545

    command line argument liyera program.ll ko content read garne vayo

--- Read_from_file.cpp
+++ Read_from_file.cpp
@@ -11,91 +11,98 @@ using namespace std;
 #define YELLOW  "\033[33m"
 #define BOLD    "\033[1m"
 
-string file_name_with_extension = "";
-string file_name_without_extension = "";
+string file_name_with_extension = ""; //this string stores the file name written in commandline without the extension
+string file_name_without_extension = ""; //this string stores the file
 string file_extension = "";
 string store_content = "";
 
-class Msg{
-    public:
-    void Error(std::string errmsg){
-        cerr<<BOLD<<"ERROR : "<<RESET<<RED<<errmsg<<RESET<<endl;
-    }
-    void Warn(std::string warnmsg){
-        cerr<<BOLD<<"WARN : "<<RESET<<YELLOW<<warnmsg<<RESET<<endl;
-    }
-};
 
-
-void check_format(int argc) {
-    if (argc != 2) {
-        cerr << BOLD << "ERROR:" << RESET << RED << " COMMAND THIK XAINA" << RESET << endl;
-
-	cerr << "Usage: " << BOLD << "llc <filename.ll>" << RESET << endl;
-        exit(1);  // Stop the program immediately
-    }
+void check_format(int argc){ /*to check if the command in the terminal is correct eg: "llc program.ll" is correct "llc" and "llc program.ll -o hello"
+                        is incorrect */
+        if (argc != 2){
+                cerr<< BOLD << "ERROR:" << RESET << RED << "COMMAND THIK XAINA" << RESET << endl;
+                //msg.Error("COMMAND THIK XAINA");
+                exit(1);
+        }
 }
 
-void check_dots() {
-    int count_dots = 0;
-    for (char c : file_name_with_extension) {
-        if (c == '.') count_dots++;
-    }
-    if (count_dots != 1) {
-        cerr << BOLD << "ERROR: " << RESET << RED << "FILE EXTENSION MILENA HAI!!!!!" << RESET << endl;
-        clog << BOLD << "WARNING:" << RESET << YELLOW << "FILE EXTENSION EG: namaste.ll HUNXA" << RESET << endl;
-        exit(1);
-    }
+
+void check_dots(){
+        int count_dots = 0;
+        for (int i = 0; i < file_name_with_extension.length(); i++){
+                if (file_name_with_extension[i] == '.')
+                        count_dots += 1;
+        }
+//More than 1 dots in file name shows errors
+        if (count_dots != 1){
+                cerr << BOLD << "ERROR: "  << RESET << RED << "FILE EXTENSION MILENA HAI!!!!!" << RESET<< endl;
+                clog << BOLD << "WARNING:" << RESET << YELLOW << "FILE EXTENSION EG: namaste.ll HUNXA" << RESET << endl;
+        }
+
+
 }
 
-void extract_filename() {
-    for (char c : file_name_with_extension) {
-        if (c != '.')
-            file_name_without_extension += c;
-        else
-            break;
-    }
+
+void extract_filename(){
+        for (int i = 0; i < file_name_with_extension.length(); i++){
+                if (file_name_with_extension[i] != '.'){
+                        file_name_without_extension += file_name_with_extension[i];
+                }
+                else
+                        break;
+        }
 }
 
-void extract_file_extension(Msg err) {
-    unsigned int dot_pos = file_name_with_extension.rfind('.');
-    if (dot_pos != string::npos) {
-        file_extension = file_name_with_extension.substr(dot_pos + 1);
-    } else {
-        err.Error("EXTENSION NAI XAINA");
-    }
+
+void extract_file_extension(){
+        for (int i = file_name_with_extension.length()-1; i >= 0; i--){
+                if (file_name_with_extension[i] != '.'){
+                        file_extension += file_name_with_extension[i];
+                }
+                else
+                        break;
+        }
+//More than 1 dots in file name shows errors
+
 }
+
+
+
+
 
 void read_file() {
-    string filename = file_name_without_extension + ".ll";
-    fstream file(filename, ios::in);
-    if (!file.is_open()) {
-        cerr << BOLD << "ERROR:" << RESET << RED << " Could not open file " << filename << RESET << endl;
-        exit(1);
-    }
-    string line;
-    while (getline(file, line)) {
-        store_content += line;
-        store_content += '\n';  // preserve newlines
-    }
-    cout << "\nStore_content:\n" << store_content << endl;
+        cout << "\n" << file_name_without_extension;
+        fstream file( file_name_without_extension + ".ll", ios::in);
+
+        if (!file.is_open()) {
+                cerr << "\nError: Could not open file " << file_name_with_extension << ".ll" << endl;
+                return;
+        }
+
+        string line;
+        while(getline(file, line)) {
+                store_content += line;
+        }
+        cout << "\n Store_content " << store_content;
 }
 
-int main(int argc, char* argv[]) {
-    Msg err;
-  // First, validate number of arguments
-    check_format(argc);
-    file_name_with_extension = argv[1];
-    check_dots();
-    // Now it's safe to use argv[1]
-    extract_filename();
-    extract_file_extension(err);
 
-    if (file_extension != "ll") {
-        cerr << BOLD << "ERROR:" << RESET << RED << " File extension must be .ll" << RESET << endl;
-        return 1;
-    }
 
-    read_file();
-    return 0;
+
+
+
+
+int main(int argc, char* argv[]){
+
+        check_format(argc);
+	file_name_with_extension = argv[1];
+        extract_filename();
+        check_dots();
+        extract_file_extension();
+        if (file_extension != "ll")
+                return 0;
+        //cout << "\t" << file_name_without_extension << endl;
+        read_file();
+        cout << store_content << endl;
+        return 0;
 }
